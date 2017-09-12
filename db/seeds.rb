3f2abe91afc979850f8c06754909a64907a2c3e0
naderae <naderabouezze93@gmail.com>
2017-09-12 15:42:30 -0400

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,11 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+  product_1 = Product.create(name: 'ball', description: 'authentic NBA ball', price_in_cents: 15)
+  product_2 = Product.create(name: 'rim', description: 'authentic NBA rim', price_in_cents: 10)
+  product_3 = Product.create(name: 'shoes', description: 'Curry 3 shoes', price_in_cents: 150)
+#
+#
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
